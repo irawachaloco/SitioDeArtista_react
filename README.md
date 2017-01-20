04f2ca0f3a1ee59a231b57df18cff4e1af279423
“irawachaloco” <irawachaloco@gmail.com>
2017-01-19 22:27:28 -0600

    subo resto de configuracion

---
+++ README.md
@@ -0,0 +1,4 @@
+# sitioDeArtista
+
+Este repositorio contendrá plantillas y estructuras básicas para preparar sitios personalizados para artistas o pequeñas galerías.
+El objetivo es aprender React.js y producir una base flexible para desarrollar y presentar multiples contenidos.
---
+++ build/public/assets/js/entry_list.js
@@ -0,0 +1,42 @@
+(function(){
+	'use strict';
+	Vue.component('entry-list', {
+	  //NOtsa, recordar que tanto vue como angular hacen una transformación al standard html
+	  //las cosas con Camelcase se transforman a '-' p.ej. globalState : global-state
+	  props: [ 'globalState'],
+	  //shortcut para v-bind es sólo ':' como abajo en el primer caso.
+	  template: '<div><ul>\
+	  <li v-for="entry in globalState.list">\
+	  <entry-item :entry="entry" v-bind:state="globalState"></entry-item>\
+	  </li>\
+	  </ul>\
+	  <div>Selected article: {{ globalState.selected.title }} </div>\
+	  <div>Date created: {{ globalState.selected.date }} </div>\
+	  <div v-html="globalState.selected.body"></div></div>'
+	});
+	Vue.component('entry-item',{
+		props: ['entry', 'state'],
+		template: '<a v-on:click="state.selected=entry" :class="cssClass"> {{ entry.title }} </a>',
+		computed: { 
+			cssClass: function(){
+				return (this.entry===this.state.selected)? 'selected' : ''
+			}
+		}
+	});
+	console.log(window.state.entryList);
+	var app = new Vue ({
+		el: '#entryList',
+		data: {
+			state: {
+				list: window.state.entryList,
+				selected:  window.state.entryList[0]
+				//por hacer:
+				//modalOpen: false
+			}
+		}
+	})
+	// var menuApp = new Vue ({
+	// 	el...
+	// })
+	//console.log(window.state.entryList[0]);
+}());
