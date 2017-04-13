066a287873b288a831d6c3f15be497ecc40a22f7
“irawachaloco” <irawachaloco@gmail.com>
2017-04-13 18:11:58 -0500

    Implementamos el primer observable

--- src/content.tsx
+++ src/content.tsx
@@ -9,24 +9,88 @@ const settings = {
         slidesToShow: 1,
         slidesToScroll: 1
       };
-export const Content = () => 
-    <div className="content">
-      <section className="home content-box-1200">
-        <div className="flex">
-          <Slider {...settings} className="slider flex-2">
-            <div className="slide-item">
-              <img src="assets/images/ecce-homo.jpg" alt=""/>
-            </div>
-            <div className="slide-item">
-              <img src="assets/images/cafe.jpg" alt=""/>
-            </div>
-          </Slider>
-          <div className="news flex-1">
-            <h2>Somme news</h2>
-            <p>Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small line of blind text by the name of Lorem Ipsum decided to leave for the far World of Grammar.</p>
-          </div>
-        </div>
-      </section>
-    </div>;
 
-window.rx = rx;
+interface IContentState {data: any[]}
+export class Content extends React.Component<undefined, IContentState> {
+  constructor() {
+    super();
+    this.state = {
+      data: [],
+    };
+
+    rx.Observable.ajax({
+      url:'/api/news'
+    })
+      .map(r => r.response)
+      .subscribe(
+        next => {
+          this.setState((prevState) => ({
+            data: next.data
+          }));
+          console.log(next.data);
+        },
+        error => {
+          console.log('error', error);
+        },
+        () => {
+          console.log('complete');
+        }
+      );
+  }
+  render() {
+      return (
+        <div className="content">
+          <section className="home content-box-1200">
+            <div className="flex">
+              <Slider {...settings} className="slider flex-2">
+                {this.state.data.length > 0 ? this.state.data.map((entry, index)=> {
+                  return (
+                    <div className="slide-item" key={index}>
+                      <h1>{ entry.title }</h1>
+                    </div>
+                    );
+                }) : <div></div>}
+              </Slider>
+              <div className="news flex-1">
+                <h2>Somme news</h2>
+                <p>Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small line of blind text by the name of Lorem Ipsum decided to leave for the far World of Grammar.</p>
+              </div>
+            </div>
+          </section>
+        </div>
+        );
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+//Jugando con observables
+/*rx.Observable.range(1,4)
+  .map( n => n*2 )
+  .flatMap( n => rx.Observable.range(1,n))
+  .filter( n => n % 2 === 0 )
+  .subscribe(
+    next => {
+      console.log('next', next);
+    },
+    error => {
+      console.log('error', error);
+    },
+    () => {
+      console.log('complete');
+    }
+  );*/
+
+  // of > unit: convierte un valor en un bolsa
+  // range, from, interval...
+
+  //básicos: of(unit) + map(devuelve otra bolsa) + flatMap(desenvuelve los objetos de la bolsa grande)
+  //Se debe cumplir reglas para estos observables (son reglas matemáticas: objetos llamados 'mónadas')
